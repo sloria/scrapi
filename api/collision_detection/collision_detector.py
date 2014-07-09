4e698264d3dc01cf3a81d67b448aedb0fe39dc57
Casey <cmr3rm@virginia.edu>
2014-07-09 09:25:32 -0400

    some changes...

--- api/collision_detection/collision_detector.py
+++ api/collision_detection/collision_detector.py
@@ -20,3 +20,11 @@ def detect(doc):
     print json.dumps(search.search('scrapi', str(bigword)))
 
 detect('collision_test/PLoS/10.1371journal.pbio.0020137/2014-07-08 14:33:06.336347/parsed.json')
+
+
+#TODO
+# collect the search results into some kinda list
+# from dat list see what search results have bigword in their title
+# if bigword is in the title
+    # check the whole title with fuzzy wuzzy to see if they are the same/close
+        # check some other stuffz (author etc)
