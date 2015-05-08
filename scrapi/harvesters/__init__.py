c75ef6c3be82634afdf24e991cef95d031050862
Fabian von Feilitzsch <fabian@fabianism.us>
2015-05-08 14:45:14 -0400

    start adding recursive imports

--- scrapi/harvesters/__init__.py
+++ scrapi/harvesters/__init__.py
@@ -4,15 +4,26 @@ NOTE: Empty folders in will cause this to break
 """
 import os
 
+dirname = os.path.dirname(__file__)
+
+
+def files(dir):
+    return next(os.walk(dir))[2]
+
+
+def folders(dir):
+    return map(lambda x: os.path.join(dir, x), next(os.walk(os.path.dirname(dir)))[1])
+
+imports = map(files, folders('scrapi/harvesters/')) #TODO
 # Get a list of folders
-_, __all__, files = next(os.walk(os.path.dirname(__file__)))
+_, __all__, files = next(os.walk(dirname))
 
 # Find all .py files that are not init
 __all__.extend([
     name[:-3]
-    for name in files
+    for name in files + imports
     if name[-3:] == '.py'
-    and name != '__init__.py'
+    and '__init__.py' not in name
 ])
 
 # Import everything in __all__
