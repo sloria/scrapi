4f4495797aa3334fcc1890d5875d22226e5aa50c
erinspace <erin.braswell@gmail.com>
2015-07-27 16:33:52 -0400

    Remove pytest ini and postgres decode pytearray

--- scrapi/processing/postgres.py
+++ scrapi/processing/postgres.py
@@ -25,6 +25,7 @@ class PostgresProcessor(BaseProcessor):
         modified_doc = copy.deepcopy(raw_doc.attributes)
         if modified_doc.get('versions'):
             modified_doc['versions'] = modified_doc['versions'].encode('utf8')
+            modified_doc['doc'] = raw_doc.doc.decode('utf8')
 
         document.raw = modified_doc
 
