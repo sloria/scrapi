d2c8820e082c577a9f6890af62a6b87c232ab946
erinspace <erin.braswell@gmail.com>
2015-10-06 15:36:58 -0400

    Add back real processing, but leave testing writing to file just in case

--- scrapi/tasks.py
+++ scrapi/tasks.py
@@ -1,5 +1,4 @@
 import logging
-import json
 import functools
 from itertools import islice
 from datetime import date, timedelta
@@ -138,12 +137,13 @@ def process_uris(async, **kwargs):
             source_buckets = util.parse_urls_into_groups(source)
             all_buckets.append(source_buckets)
 
-    with open('all_sources.json', 'w') as outfile:
-        json.dump(all_buckets, outfile)
+    # import json
+    # with open('all_sources.json', 'w') as outfile:
+    #     json.dump(all_buckets, outfile)
 
-    # for source_dict in all_buckets:
-    #     for group in source_dict['uris']:
-    #         process_uris_at_one_base_uri.delay(group['individual_uris'], async, kwargs=kwargs)
+    for source_dict in all_buckets:
+        for group in source_dict['uris']:
+            process_uris_at_one_base_uri.delay(group['individual_uris'], async, kwargs=kwargs)
 
 
 @task_autoretry(default_retry_delay=settings.CELERY_RETRY_DELAY, max_retries=0)
