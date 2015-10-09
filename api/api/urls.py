665cba52cc879fa29212696eb0ae25517fcd83c6
erinspace <erin.braswell@gmail.com>
2015-10-09 12:13:41 -0400

    Add registration urls to main proj

--- api/api/urls.py
+++ api/api/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('api.webview.urls')),
+    url(r'^api/', include('api.webview.urls')),
+    url(r'^registration', include('api.provider_registration.urls', namespace="provider_registration"))
 ]
