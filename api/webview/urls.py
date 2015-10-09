edd77d06c4d2993e2cb3a2e54dbb02dcc2cc7a2f
erinspace <erin.braswell@gmail.com>
2015-10-09 13:29:10 -0400

    Add pushedurls to granular webview app

--- api/webview/urls.py
+++ api/webview/urls.py
@@ -4,5 +4,10 @@ from api.webview import views
 urlpatterns = [
     url(r'^documents/$', views.DocumentList.as_view()),
     url(r'^documents/(?P<source>\w+)/$', views.DocumentsFromSource.as_view(), name='source'),
-    url(r'^documents/(?P<source>[a-z]+)/(?P<docID>(.*))/$', views.document_detail, name='document_detail')
+    url(r'^documents/(?P<source>[a-z]+)/(?P<docID>(.*))/$', views.document_detail, name='document_detail'),
+    url(r'^posted/', views.DataList.as_view()),
+    url(r'^established/$', views.EstablishedDataList.as_view()),
+    url(r'^(?P<pk>[0-9]+)/$', views.DataDetail.as_view(), name='data-detail'),
+    url(r'^get-api-key/$', views.render_api_form, name='get-api-key'),
+    url(r'^help/$', views.render_api_help, name='help'),
 ]
