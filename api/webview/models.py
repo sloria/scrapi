eabf5d7f25b5979e2f3cd7dbf2502cbdd09f39b7
erinspace <erin.braswell@gmail.com>
2015-10-09 13:28:30 -0400

    Add pushed data models to api models

--- api/webview/models.py
+++ api/webview/models.py
@@ -1,5 +1,10 @@
 from django.db import models
+from django.conf import settings
+from django.dispatch import receiver
 from django_pgjson.fields import JsonField
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from rest_framework.authtoken.models import Token
 
 
 class Person(models.Model):
@@ -44,3 +49,31 @@ class Document(models.Model):
 
     contributors = models.ManyToManyField(Person, related_name='documents')
     urls = models.ManyToManyField(URL, related_name='urls')
+
+
+class Provider(models.Model):
+    user = models.OneToOneField(User)
+    established = models.BooleanField(default=False)
+
+
+class PushedData(models.Model):
+    jsonData = JsonField(null=True)
+    source = models.ForeignKey('auth.User', related_name='data')
+    collectionDateTime = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def established(self):
+        return self.source.provider.established
+
+    @classmethod
+    def fetch_established(cls):
+        return cls.objects.all().filter(source__provider__established=True)
+
+    class Meta:
+        verbose_name = 'Data Object'
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
