9f8b4d1f2a534199ee39141f5d4d2ea2285fe28e
erinspace <erin.braswell@gmail.com>
2015-10-19 16:21:27 -0400

    Add query orcid helper

--- scrapi/processing/helpers.py
+++ scrapi/processing/helpers.py
@@ -14,3 +14,15 @@ def save_status_of_uri(uri, uritype):
         'resolved_datetime': datetime.datetime.now(),
         'resolved_status': uri_status.status_code,
     }
+
+
+def query_orcid_api(id_email):
+    orcid_email_query = 'http://pub.orcid.org/v1.2/search/orcid-bio?q=email:{}'.format(id_email)
+
+    orcid_results = requests.get(orcid_email_query, headers={'Accept': 'application/orcid+json'}).json()
+    number_results = orcid_results['orcid-search-results']['num-found']
+
+    if number_results > 0:
+        return number_results
+    else:
+        return None
