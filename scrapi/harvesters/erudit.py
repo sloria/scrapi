69bcee36243bf2678b3e4d20272c35e2d57e8434
Katherine Schinkel <kms6bn@virginia.edu>
2015-12-14 16:37:19 -0500

    added Érudit

--- scrapi/harvesters/erudit.py
+++ scrapi/harvesters/erudit.py
@@ -1,8 +1,7 @@
-# coding=utf-8
 '''
 Harvester for the Erudit for the SHARE project
 
-Example API call: http://oai.erudit.org/oai/request?verb=ListRecords&metadataPrefix=oai_dc
+Example API call: http://oai.erudit.org/oai/?verb=ListRecords&metadataPrefix=oai_dc
 '''
 from __future__ import unicode_literals
 
@@ -11,9 +10,9 @@ from scrapi.base import OAIHarvester
 
 class EruditHarvester(OAIHarvester):
     short_name = 'erudit'
-    long_name = 'Érudit'
-    url = 'http://erudit.org'
+    long_name = 'Erudit'
+    url = 'http://oai.erudit.org'
 
-    base_url = 'http://oai.erudit.org/oai/request'
-    property_list = ['date', 'type', 'identifier', 'relation', 'rights', 'setSpec']
+    base_url = 'http://oai.erudit.org/oai/'
+    property_list = ['date', 'rights', 'identifier', 'relation', 'type', 'setSpec']
     timezone_granularity = True
