e4f91a53ad1472484078eae425e64fd759f52f39
Fabian von Feilitzsch <fabian@fabianism.us>
2015-12-22 16:46:10 -0500

    Implement basic data shunting routines

--- scrapi/processing/jamdb.py
+++ scrapi/processing/jamdb.py
@@ -1,11 +1,14 @@
 from __future__ import absolute_import
 
+import copy
 import logging
+import hashlib
+import requests
 
-from scrapi import events, requests
-from scrapi.linter import RawDocument, NormalizedDocument
-from scrapi.processing import DocumentTuple
-from scrapi.processing.base import BaseProcessor, FakeDatabaseManager
+from scrapi import events
+# from scrapi.linter import RawDocument, NormalizedDocument
+# from scrapi.processing import DocumentTuple
+from scrapi.processing.base import BaseProcessor, FakeDataBaseManager
 
 logger = logging.getLogger(__name__)
 
@@ -13,27 +16,65 @@ logger = logging.getLogger(__name__)
 class JamDBProcessor(BaseProcessor):
     NAME = 'jamdb'
 
-    manager = FakeDatabaseManager()
+    manager = FakeDataBaseManager()
+    base_url = 'http://localhost:1212/v1/'
+    namespace = 'SHARE'
+    token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjI0NDc3Nzk5MDgsInN1YiI6InRyYWNrZWQtU0hBUkV8dXNlcnMtY2hyaXMifQ.kVvJaZcIDDIzNb5hyey_7YqsrfZurZcqMH65aRysq_4'
+
+    @property
+    def collections(self):
+        return {
+            'documents': '{}-data'.format(self.namespace.lower()),
+            'contributors': '{}-contributor'.format(self.namespace.lower())
+        }
+
+    @property
+    def cookies(self):
+        return {'cookies': self.token}
+
+    def url_for(self, type_):
+        return '{}namespaces/{}/collections/{}/documents'.format(self.base_url, self.namespace, self.collections[type_])
+
+    def format_data(self, attributes, source, docID, type_):
+        return {
+            'data': {
+                'id': self.get_id(source, docID),
+                'type': type_,
+                'attributes': attributes
+            }
+        }
+
+    def get_id(self, source, docID):
+        return hashlib.sha1(
+            '{}|{}'.format(source, docID).encode('utf-8')
+        ).hexdigest()
+
+    def upsert(self, url, data):
+        response = requests.put(url, cookies=self.cookies, json=data)
+        if response.status_code == 404:
+            response = requests.post(url, cookies=self.cookies, json=data)
+        return response
 
     @events.logged(events.PROCESSING, 'raw.jamdb')
     def process_raw(self, raw_doc):
-        requests.post(
-            '{}/namespaces/{}/collections/{}/documents'.format(
-                self.base_url, self.namespace, self.collection
-            ),
-            cookie=self.cookie,
-            json={
-                'data': {
-                    'id': self.get_id(raw_doc['source'], raw_doc['docID']),
-                    'type': 'documents',
-                    'attributes': raw_doc.attributes
-                }
-            }
-        )
+        data = self.format_data({'raw': raw_doc.attributes}, raw_doc['source'], raw_doc['docID'], 'documents')
+        self.upsert(self.url_for('documents'), data)
 
     @events.logged(events.PROCESSING, 'normalized.jamdb')
     def process_normalized(self, raw_doc, normalized):
-        raise NotImplementedError
+        normalized = self.process_contributors(raw_doc, normalized)
+        data = self.format_data({'normalized': normalized.attributes}, raw_doc['source'], raw_doc['docID'], 'documents')
+        self.upsert(self.url_for('documents'), data)
+
+    def process_contributors(self, raw_doc, normalized):
+        normalized = copy.deepcopy(normalized)
+        id_ = self.get_id(raw_doc['source'], raw_doc['docID'])
+        for i, contributor in enumerate(normalized['contributors']):
+            contributor['researchObjectId'] = id_
+            data = self.format_data(contributor, raw_doc['source'], raw_doc['docID'], 'contributors')
+            self.upsert(self.url_for('contributors'), data)
+            normalized['contributors'][i]['id'] = id_
+        return normalized
 
     def documents(self, *sources):
         raise NotImplementedError
