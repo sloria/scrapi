7576fce549e0fb5fd99c4de443adad915f4b6756
jgw4sq <~jgw4sq@virginia.edu>
2016-03-10 16:56:17 -0500

    Altered the nist harvester schema to fix formmating of the tags

--- scrapi/harvesters/nist.py
+++ scrapi/harvesters/nist.py
@@ -6,6 +6,8 @@ Example API call: https://materialsdata.nist.gov/dspace/oai/request?verb=ListRec
 from __future__ import unicode_literals
 
 from scrapi.base import OAIHarvester
+from scrapi.base.helpers import updated_schema, default_name_parser
+
 
 
 class NistHarvester(OAIHarvester):
@@ -16,3 +18,29 @@ class NistHarvester(OAIHarvester):
     base_url = 'https://materialsdata.nist.gov/dspace/oai/request'
     property_list = ['relation', 'rights', 'identifier', 'type', 'date', 'setSpec']
     timezone_granularity = True
+    @property
+    def schema(self):
+        return updated_schema(self._schema, {'subjects': ('//dc:subject/node()', format_tags)})
+
+def format_tags(all_tags):
+    tags = []
+    for tag in all_tags:
+        tag = tag.replace("Computational File Repository Categories",'')
+        tag = tag.replace("Computational File Repository",'')
+        tag = tag.replace("File Repository Categories",'')
+
+        if "::" in tag:
+            tags.extend(tag.split("::"))
+        if "," in tag:
+            tags.extend(tag.split(","))
+        elif "::" not in tag:
+            tags.append(tag)
+
+
+    for tag in tags:
+        if "::" in tag:
+            tags.remove(tag)
+        if tag =="":
+            tags.remove(tag)
+    return tags
+
