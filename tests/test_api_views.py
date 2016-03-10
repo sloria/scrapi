93bd567cefcfe2faeeb58a0165756bb6a112a94d
jgw4sq <~jgw4sq@virginia.edu>
2016-03-10 13:22:57 -0500

    Created new view to be able to display specific documents based on start and end dates in the url. Wrote test case to confirm that this is working

--- tests/test_api_views.py
+++ tests/test_api_views.py
@@ -2,10 +2,10 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.api.settings")
 
 import django
-from django.test import TestCase
-from rest_framework.test import APIRequestFactory
-
-from api.webview.views import DocumentList, status, institutions
+from django.test import TestCase 
+from rest_framework.test import APIRequestFactory, APITestCase, APIClient, force_authenticate
+from dateutil.parser import parse
+from api.webview.views import DocumentList, status, institutions, DocumentsByProviderUpdatedDateTime
 from api.webview.models import Document
 
 django.setup()
@@ -71,8 +71,26 @@ class APIViewTests(TestCase):
         response = view(request)        
         self.assertNotContains(response, "bad",
                             status_code=200)
+        self.assertContains(response, "good", status_code=200)
+        print(response)
 
 
+class APIViewTests2(APITestCase):
+
+    def test_query_search_by_providerupdatedtime(self):
+        view = DocumentsByProviderUpdatedDateTime.as_view()
+        create_new_document(source = "tooearly",providerUpdatedDateTime= parse("2012-01-01"))
+        create_new_document(source = "rightontime", providerUpdatedDateTime= parse("2013-01-05"))
+        create_new_document(source= "toolate", providerUpdatedDateTime = parse("2015-01-01"))
+        response = self.client.get('/documents/2013-01-01&2014-12-30/', kwargs= {'from':"2013-01-01",'until':'2014-12-30'})
+        force_authenticate(response)
+        self.assertNotContains(response, "tooearly", status_code=200)
+        self.assertContains(response, "rightontime", status_code=200)
+        self.assertNotContains(response, "toolate", status_code =200)
+
+
+def create_new_document(source,providerUpdatedDateTime):
+        return Document.objects.create(source = source, providerUpdatedDateTime = providerUpdatedDateTime)
 def create_document(source,normalized):
         return Document.objects.create(source= source,normalized= normalized)
 
