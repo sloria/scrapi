2c6478b50bcb191ab2c8391350e950f61843b524
Katherine Schinkel <kms6bn@virginia.edu>
2016-06-21 20:03:13 -0700

    add test for ucar

--- tests/test_harvesters.py
+++ tests/test_harvesters.py
@@ -4,7 +4,6 @@ import vcr
 import mock
 import pytest
 
-from freezegun import freeze_time
 from scrapi import base
 from scrapi import registry, requests
 
@@ -20,7 +19,6 @@ def mock_maybe_load_response(monkeypatch):
     monkeypatch.setattr(requests, '_maybe_load_response', mock_mlr)
     monkeypatch.setattr(requests.HarvesterResponse, 'save', mock_save)
 
-@freeze_time("2016-05-13")
 @pytest.mark.parametrize('harvester_name', filter(lambda x: x != 'test', sorted(map(str, registry.keys()))))
 def test_harvester(monkeypatch, harvester_name, *args, **kwargs):
     monkeypatch.setattr(requests.time, 'sleep', lambda *_, **__: None)
@@ -28,7 +26,7 @@ def test_harvester(monkeypatch, harvester_name, *args, **kwargs):
 
     harvester = registry[harvester_name]
 
-    with vcr.use_cassette('tests/vcr/{}.yaml'.format(harvester_name), match_on=['host'], record_mode='once'):
+    with vcr.use_cassette('tests/vcr/{}.yaml'.format(harvester_name), match_on=['host'], record_mode='none'):
         harvested = harvester.harvest()
         assert len(harvested) > 0
 
